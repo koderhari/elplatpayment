00b3d6e026dc380e26782f9aaed7226d89172f15
koderhari <koderhari@gmail.com>
2016-04-30 17:01:08 +0300

    all logic out of main plugin dll

--- GM_PaymentsPlugin.Forms/Infrastructure/Commands/CityCommand.cs
+++ GM_PaymentsPlugin.Forms/Infrastructure/Commands/CityCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -15,6 +17,7 @@ namespace ElPlat_PaymentsPlugin.Forms.Infrastructure.Commands
         protected static char delimiterL3 = Convert.ToChar(3);
         protected static string counterHeader = "#СЧ";
         protected static string addInfoHeader = "#ДС";
+        protected static byte ZipFlag = (byte)33;
         
         public string ErrorMessage { get; set; }
         public bool Success { get; set; }
@@ -54,6 +57,8 @@ namespace ElPlat_PaymentsPlugin.Forms.Infrastructure.Commands
             List<byte> allBytes = new List<byte>();
             byte[] bytes = null;
             bool flag = true;
+
+
             while (flag)
             {
                 bytes = new byte[1000];
@@ -69,11 +74,60 @@ namespace ElPlat_PaymentsPlugin.Forms.Infrastructure.Commands
                 }
 
             }
+
+            if (CheckCompressedBytes(allBytes))
+                allBytes = UnCompressBytes(allBytes);
             var result =Encoding.GetEncoding(866).GetString(allBytes.ToArray(), 0, allBytes.Count);
             CheckError(result);
             return result;
         }
 
+
+        private bool CheckCompressedBytes(List<byte> allBytes)
+        {
+            if (allBytes.Count < 1) return false;
+            if (allBytes[0] == ZipFlag) return true;
+            return false;
+        }
+
+        private List<byte> UnCompressBytes(List<byte> allBytes)
+        {
+            byte[] data=new byte[allBytes.Count - 1];
+            allBytes.CopyTo(1, data, 0, data.Length);
+            return Decompress(data).ToList();
+
+        }
+
+
+        public byte[] UnZipStr(byte[] input)
+        {
+            using (MemoryStream inputStream = new MemoryStream(input))
+            {
+                using (DeflateStream gzip =
+                  new DeflateStream(inputStream, CompressionMode.Decompress))
+                {
+                    using (var resultStream = new MemoryStream())
+                    {
+                        gzip.CopyTo(resultStream);
+                        return resultStream.ToArray();
+                    }
+                }
+            }
+        }
+        
+
+        static byte[] Decompress(byte[] data)
+        {
+           
+            using (var compressedStream = new MemoryStream(data))
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (var resultStream = new MemoryStream())
+            {
+                zipStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+        
 
 
         public void Dispose()
